188afde1b2b2e62620f6433a579199a84690d31c
Liam Reed <liamdreed@gmail.com>
2014-12-10 17:05:44 +0000

    End of day 10/12/2014 - Complete

--- ripper.py
+++ ripper.py
@@ -1,5 +1,8 @@
 import urllib2 #urllib2 required for fetching information from ripURL
-import urllib
+import requests #for downloading files
+import os #for directory path
+from PIL import Image
+import time #delay actions so it doesnt freeze
 import mtTkinter as tk #import mtTkinter for GUI components
 import tkFileDialog #import tkFileDialog for Open Directory window
 from BeautifulSoup import * #import BeautifulSoup library for HTML scraping
@@ -14,13 +17,16 @@ class siteRipper(tk.Tk): #init window
     def doRip(self):
         self.LB1.delete(0, tk.END)
         self.ripURL = self.E1.get() #get text from Entry 1
-        if self.ripURL != None: #check if ripURL is empty
+        self.chosenPath = self.E2.get() #get path from Entry 2
+        if self.ripURL != "": #check if ripURL is empty
             self.getHTML() #get HTML source of page
-            self.getCSS() #get full CSS links from page
-            self.getJS() #get full JS links from page
-            self.getIMG() #get full IMG links from page
+            self.getCSS() #get full CSS urls from page
+            self.getJS() #get full JS urls from page
+            self.getIMG() #get full IMG urls from page
         else:
-            self.L3.text = "URL field was empty" #report error to user via L3 label
+            errortext = "URL field was empty"
+            self.L3.config(text= "URL field was empty") #report error to user via L3 label
+            print errortext
 
     def create_interface(self):
         self.geometry("950x580+150+150") #set size of window
@@ -29,22 +35,23 @@ class siteRipper(tk.Tk): #init window
         L2 = tk.Label(text="Output Directory")
         L2.place(x=2, y=26)
         self.L3 = tk.Label() #error message label
-        self.L3.place(x=140, y=210)
+        self.L3.place(x=475, y=547)
         self.E1 = tk.Entry(self, width=135) #URL input field
         self.E1.place(x=120, y=6)
         self.E2 = tk.Entry(self, width=130) #Directory path field
         self.E2.place(x=150, y=30)
         self.LB1 = tk.Listbox(self, width=135, height=30)
         self.LB1.place(x=120, y=52)
+        self.chosenPath = './'
         B1 = tk.Button(self, text="Dir", command=self.saveDialog) #Dir button fires saveDialog method
         B1.place(x=120, y=26)
         B2 = tk.Button(self, text="Download", command=self.doRip) #Download button fires doRip method
         B2.place(x=870, y=540)
         
     def saveDialog(self):
-        path = tkFileDialog.askdirectory()
-        self.E1.text = path
-        return path
+        self.chosenPath = tkFileDialog.askdirectory(initialdir='.')
+        print self.chosenPath
+        self.E2.insert(0, self.chosenPath)
 
     def getHTML(self):
         print self.ripURL
@@ -52,57 +59,104 @@ class siteRipper(tk.Tk): #init window
         site = urllib2.urlopen(self.ripURL) #open URL inputted in E1 Entry
         data = site.read()
         self.soup = BeautifulSoup(data)
-        file = open('source.txt', 'w')
+        if not os.path.exists(self.chosenPath):
+            os.makedirs(self.chosenPath)
+        print self.chosenPath + "/source.txt"
+        file = open(self.chosenPath + '/source.txt', 'w')
         file.writelines(data) #save HTML source to disk
         file.close()
-        print data #print HTML to console (debug)
+        time.sleep(0.4)
+        print "Saved page source to disk - source.txt" #print complete message to console
 
     def getCSS(self):
-        css_files = self.soup.findAll('link',{'rel':'stylesheet'}) #find links to CSS files
+        print "Starting Asset Rip... [May become unresponsive]" #Give user feedback on process
+        css_files = self.soup.findAll('link',{'rel':'stylesheet'}) #find urls to CSS files
         for css in css_files:
-            css = self.ripURL + css['href']
-            self.LB1.insert(tk.END, css) #add CSS links to LB1 listbox
-            print css #print CSS links to console (debug)
-            link = css
-            self.downloadFile(link)
+            if css['href'].startswith("//"):
+                css = css['href'].replace('//', 'http://')
+                self.LB1.insert(tk.END, css) #add CSS urls to LB1 listbox
+                print css #print CSS urls to console (debug)
+                url = css
+                self.downloadFile(url, path= self.chosenPath + '/css/')
+                time.sleep(0.1)
+            elif css['href'].startswith("http"):
+                css = css['href']
+                self.LB1.insert(tk.END, css) #add CSS urls to LB1 listbox
+                print css #print CSS urls to console (debug)
+                url = css
+                self.downloadFile(url, path= self.chosenPath + '/css/')
+                time.sleep(0.1)
+            else:
+                css = self.ripURL + css['href']
+                self.LB1.insert(tk.END, css) #add CSS urls to LB1 listbox
+                print css #print CSS urls to console (debug)
+                url = css
+                self.downloadFile(url, path= self.chosenPath + '/css/')
+                time.sleep(0.1)
 
     def getJS(self):
-        js_files = self.soup.findAll('script',{'src':True}) #find links to JS files and filter only the src attribute
+        js_files = self.soup.findAll('script',{'src':True}) #find urls to JS files and filter only the src attribute
         for js in js_files:
             if js['src'].startswith("//"): #check if relative or absolute URL to JS file
-                js = js['src']
+                js = js['src'].replace('//', 'http://')
                 self.LB1.insert(tk.END, js) #add to LB1 listbox
-                print js #print JS links to console (debug)
-                link = js
-                self.downloadFile(link)
+                print js #print JS urls to console (debug)
+                url = js
+                self.downloadFile(url, path= self.chosenPath + '/js/')
+                time.sleep(0.1)
             else:
-                self.LB1.insert(tk.END, js) #add links to LB1 listbox
+                self.LB1.insert(tk.END, js) #add urls to LB1 listbox
                 js = self.ripURL + js['src']
-                print js #print JS links to console (debug)
-                link = js
-                self.downloadFile(link)
+                print js #print JS urls to console (debug)
+                url = js
+                self.downloadFile(url, path= self.chosenPath + '/js/')
+                time.sleep(0.1)
 
     def getIMG(self):
-        img_files = self.soup.findAll('img',{'src':True}) #find all links to images on page
+        img_files = self.soup.findAll('img',{'src':True}) #find all urls to images on page
         for img in img_files:
-            if img['src'].startswith('http'): #check if img link is relative or absolute
+            if img['src'].startswith('http'): #check if img url is relative or absolute
                 img = img['src']
-                self.LB1.insert(tk.END, img) #add img link to LB1 listbox
-                print img #print img links to console (debug)
-                link = img
-                self.downloadFile(link)
+                self.LB1.insert(tk.END, img) #add img url to LB1 listbox
+                print img #print img urls to console (debug)
+                url = img
+                self.downloadFile(url, path= self.chosenPath + '/img/')
+                time.sleep(0.1)
+            elif img['src'].startswith('//'): #check if img url is relative or absolute
+                img = img['src'].replace('//', 'http://')
+                self.LB1.insert(tk.END, img) #add img url to LB1 listbox
+                print img #print img urls to console (debug)
+                url = img
+                self.downloadFile(url, path= self.chosenPath + '/img/')
+                time.sleep(0.1)
             else:
                 img = self.ripURL + img['src']
                 self.LB1.insert(tk.END, img) #add to LB1 listbox
-                print img #print img links to console (debug)
-                link = img
-                self.downloadFile(link)
+                print img #print img urls to console (debug)
+                url = img
+                self.downloadFile(url, path= self.chosenPath + '/img/')
+                time.sleep(0.1)
 
-    def downloadFile(self, link):
-        try:
-            self.currentFile = urllib.urlretrieve(link)
-        except:
-            self.L3.text = "Error downloading file"
+    def createFilename(self, url, name, path):
+        dotSplit = url.split('.')
+        if not os.path.exists(path):
+            os.makedirs(path)
+        if name == None:
+            # use the same as the url
+            slashSplit = dotSplit[-2].split('/')
+            name = slashSplit[-1]
+        ext = dotSplit[-1]
+        file = '{}{}.{}'.format(path, name, ext)
+        return file
+
+    def downloadFile(self, url, name=None, path='./'):
+        file = self.createFilename(url, name, path)
+        with open(file, 'wb') as f:
+            r = requests.get(url, stream=True)
+            for block in r.iter_content(1024):
+                if not block:
+                    break
+                f.write(block)
 
 app = siteRipper() #call main instance
 app.mainloop() #call mainloop
---
+++ ripper.spec
@@ -0,0 +1,22 @@
+# -*- mode: python -*-
+a = Analysis(['ripper.py'],
+             pathex=['C:\\Users\\REE12414772\\Documents\\pythonripper'],
+             hiddenimports=[],
+             hookspath=None,
+             runtime_hooks=None)
+pyz = PYZ(a.pure)
+exe = EXE(pyz,
+          a.scripts,
+          exclude_binaries=True,
+          name='ripper.exe',
+          debug=False,
+          strip=None,
+          upx=True,
+          console=True )
+coll = COLLECT(exe,
+               a.binaries,
+               a.zipfiles,
+               a.datas,
+               strip=None,
+               upx=True,
+               name='ripper')
